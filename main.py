53416ef2a590697c064b069224e317273223d552
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-12-12 12:09:36 +0900

    feat: カラーパレットを指定できるようになった

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 
 from pyboy import PyBoy, WindowEvent
 
-# TODO: 入力を受け付けるようにする
 
 # 🟥🟧🟨🟩🟦🟪🟫⬛⬜
 # dotにすると明らかにFPSが高い（2倍近く速い）
@@ -13,41 +12,52 @@ color_parrets = {"hot": ["⬛", "🟨", "🟧", "🟫"],
                  "cool": ["🟨", "🟨", "🟪", "🟦"],
                  "mono": ["⬛", "⬛", "⬜", "⬜"],
                  "dot": ["  ", "⠒⠒", "⠛⣤", "⣿⣿"]}
-parret = color_parrets["cool"]
-# pythonでは配列アクセスが関数呼び出し並みに遅いのであらかじめ変数にしておく
-white, light_gray, dark_gray, black = parret
 
 # メモ：実行のとき10回くらい [ctrl -]を押すといい感じ
 # Check if the ROM is given through argv
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     filename = sys.argv[1]
+    parret_name = sys.argv[2]
+elif len(sys.argv) > 1:
+    filename = sys.argv[1]
+    parret_name = "hot"
 else:
     print("Usage: python mario_boiler_plate.py [ROM file]")
     exit(1)
 
+parret = color_parrets[parret_name]
+# pythonでは配列アクセスが関数呼び出し並みに遅いのであらかじめ変数にしておく
+white, light_gray, dark_gray, black = parret
+
 headless = True
-pyboy = PyBoy(
+pb = PyBoy(
     gamerom_file=filename,
     window_type="headless" if headless else "SDL2",
     debug=False
 )
-pyboy.set_emulation_speed(1)  # 実機と同じ速度
+pb.set_emulation_speed(1)  # 実機と同じ速度
 # 全フレームのうち描画をする割合、{1//skip_rate}に一回程度描画
-# skip_rate=1/7.5 で30fps程度
-skip_rate = 1/7.5
+# skip_rate=1/11.2 で30fps程度
+skip_rate = 1/11.2
 
 height = 160
 perf_times = []
-for frame in range(1000):
+frame_count = 0
+while True:
     start_time = time.perf_counter()
-    pyboy.tick()
-    if frame % (1//skip_rate):
+
+    pb.tick()
+
+    frame_count += 1
+
+    if frame_count % (1//skip_rate):
         continue
+
     # observation: 画面のRGB値(144, 160, 3)
-    observation = np.asarray(pyboy.botsupport_manager(
+    observation = np.asarray(pb.botsupport_manager(
     ).screen().screen_ndarray(), dtype=np.uint8)
 
-    # グレースケールに (144, 160)
+    # グレースケールに変換 (144, 160)
     observation = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
 
     screen = ""
@@ -70,9 +80,10 @@ for frame in range(1000):
     sys.stdout.flush()
 
     end_time = time.perf_counter()
-    if frame < 100:  # exclude pyboy's title logo from performance time
+    if frame_count < 100:  # exclude pyboy's title logo from performance time
         continue
     perf_times.append(end_time - start_time)
+
 
 sys.stdout.write("\033[{}B".format(height))
 
